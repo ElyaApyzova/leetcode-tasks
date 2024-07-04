ce7f1a8874bf951bc66eec3e49cd523e42ea6ead
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-04 22:54:28 +0300

    completed

--- app.js
+++ app.js
@@ -78,3 +78,25 @@ const generateMatrix = function (n) {
   }
   return result;
 };
+
+//Input: n = 3, k = 3
+//Output: "213"
+
+const getPermutation = function (n, k) {
+  let factorials = new Array(n);
+  let nums = ["1"];
+  factorials[0] = 1;
+  for (let i = 1; i < n; ++i) {
+    factorials[i] = factorials[i - 1] * i;
+    nums.push((i + 1).toString());
+  }
+  --k;
+  let output = "";
+  for (let i = n - 1; i > -1; --i) {
+    let idx = Math.floor(k / factorials[i]);
+    k -= idx * factorials[i];
+    output += nums[idx];
+    nums.splice(idx, 1);
+  }
+  return output;
+};
