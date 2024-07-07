6ac27bf3b8defd394dbb05bfc3212cede3eb6b6a
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-07 21:50:12 +0300

    completed

--- app.js
+++ app.js
@@ -226,3 +226,38 @@ const uniquePaths = function (m,n) {
   }
   return uniquePaths(m - 1, n) + uniquePaths(m, n - 1)
 };
+
+
+//Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+//Output: 2
+//Explanation: There is one obstacle in the middle of the 3x3 grid above.
+//There are two ways to reach the bottom-right corner:
+//1. Right -> Right -> Down -> Down
+//2. Down -> Down -> Right -> Right
+
+
+const uniquePathsWithObstacles = function (obstacleGrid) {
+  let R = obstacleGrid.length;
+  let C = obstacleGrid[0].length;
+  if (obstacleGrid[0][0] == 1) {
+    return 0;
+  }
+
+  obstacleGrid[0][0] = 1;
+  for (let i = 1; i < R; i++) {
+    obstacleGrid[i][0] = obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1 ? 1 : 0;
+  }
+  for (let i = 1; i < C; i++) {
+    obstacleGrid[0][i] = obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1 ? 1 : 0;
+  }
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (obstacleGrid[i][j] == 0) {
+        obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+      } else {
+        obstacleGrid[i][j] = 0;
+      }
+    }
+  }
+  return obstacleGrid[R - 1][C - 1]
+};
