0befc4a0282458c1b8c4bc8ed857de4a1ef3df2a
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-09 22:38:04 +0300

    completed

--- app.js
+++ app.js
@@ -325,3 +325,29 @@ const isNumber = function (s) {
   }
   return seenDigit;
 };
+
+
+
+
+//https://leetcode.com/problems/plus-one/description/
+
+
+//Input: digits = [1,2,3]
+//Output: [1,2,4]
+//Explanation: The array represents the integer 123.
+//Incrementing by one gives 123 + 1 = 124.
+//Thus, the result should be [1,2,4].
+
+const plusOne = function (digits) {
+  let n = digits.length;
+  for (let i = n - 1; i >= 0; --i) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+  }
+  }
+  digits.unshift(1);
+  return digits;
+};
