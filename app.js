73f0d7d3c7be6f55635e255abf07659a241aebdc
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-09 22:56:58 +0300

    completed

--- app.js
+++ app.js
@@ -351,3 +351,30 @@ const plusOne = function (digits) {
   digits.unshift(1);
   return digits;
 };
+
+
+// https://leetcode.com/problems/add-binary/description/
+
+
+//Input: a = "11", b = "1"
+//Output: "100"
+
+
+const addBinary = function (a, b) {
+  let n = a.length,
+      m = b.length;
+      if (n < m) return addBinary(b, a);
+
+      let result = [];
+      let carry = 0,
+        j = m - 1;
+        for (let i = n - 1; i >= 0; --i) {
+          if (a[i] === "1") ++carry;
+          if (j >= 0 && b[j--] === "1") ++carry;
+
+          result.push((carry % 2).toString());
+          carry = Math.floor(carry / 2);
+        }
+        if (carry === 1) result.push("1");
+        return result.reverse().join("");
+};
