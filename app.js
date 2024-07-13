2f1d4fcc33bfda49b7a0847838e454c20463f01b
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-13 23:03:49 +0300

    completed

--- app.js
+++ app.js
@@ -569,3 +569,44 @@ const minDistance = function (word1, word2) {
       }
       return minDistanceRecur(word1, word2, word1.length, word2.length);
 };
+
+
+//https://leetcode.com/problems/set-matrix-zeroes/
+
+//Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+const setZeroes = function (matrix) {
+  let isCol = false;
+  let R = matrix.length;
+  let C = matrix[0].length;
+  for (let i = 0; i < R; i++) {
+    if (matrix[i][0] == 0) {
+      isCol = true;
+    }
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][j] == 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+  for (let i = 1; i < R; i++) {
+    for (let j = 1; j < C; j++) {
+      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+  if (matrix[0][0] == 0) {
+    for (let j = 0; j < C; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+  if (isCol) {
+    for (let i = 0; i < R; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+};
+
