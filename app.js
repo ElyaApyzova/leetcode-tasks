8a1a4231ba9a5228d54a0f9156c51a0f162d1153
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-13 22:07:52 +0300

    completed

--- app.js
+++ app.js
@@ -497,3 +497,16 @@ const climb_Stairs = function (i, n) {
 
 //The trailing slash should be removed.
 
+const simplifyPath = function (path) {
+  let stack = [];
+  for (let portion of path.split("/")) {
+    if (portion === "..") {
+      if (stack.length) {
+        stack.pop();
+      }
+    } else if (portion !== "." && portion) {
+      stack.push(portion);
+    }
+  }
+  return "/" + stack.join("/");
+};
