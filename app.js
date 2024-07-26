d63a4ff533d35a74ead1387fd129356da6f7e58e
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-26 23:31:51 +0300

    completed

--- app.js
+++ app.js
@@ -610,3 +610,34 @@ const setZeroes = function (matrix) {
   }
 };
 
+
+
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+
+//Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+//Output: true
+
+
+const searchMatrix = function (matrix, target) {
+  let m = matrix.length;
+  if (m == 0) return false;
+  let n = matrix[0].length;
+  let left = 0,
+  right = m * n - 1;
+  let pivotIdx, pivotElement;
+  while (left <= right) {
+    pivotIdx = Math.floor((left + right) / 2);
+    pivotElement = matrix[Math.floor(pivotIdx / n)][pivotIdx % n];
+    if (target == pivotElement) return true;
+   else {
+    if (target < pivotElement) right = pivotIdx - 1;
+    else left = pivotIdx + 1;
+  }
+}
+  return false;
+}
+
+
+
+
