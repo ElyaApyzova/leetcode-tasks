9d710c3a7152607c7c2a447ea8685634ba305eb9
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-29 22:46:42 +0300

    completed

--- app.js
+++ app.js
@@ -778,4 +778,54 @@ const subsets = function (nums) {
 
 
 
+//79  https://leetcode.com/problems/word-search/description/
+
+
+//Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], //word = "ABCCED"
+//Output: true
+
+
+const exist = function (board, word) {
+  const ROWS = board.length;
+  const COLS = board[0].length;
+  const backtrack = function (row, col, suffix) {
+    if (suffix.length == 0) return true;
+    if (
+      row < 0 ||
+      row == ROWS ||
+      col < 0 ||
+      col == COLS ||
+      board[row][col] != suffix.charAt(0)
+    )
+   return false;
+    let ret = false;
+    board[row][col] = "#" 
+    const directions = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0]
+    ];
+    
+    for (let [rowOffset, colOffset] of directions) {
+      ret = backtrack(row + rowOffset, col + colOffset, suffix.slice(1));
+      if (ret) break;
+    }
+    board[row][col] = suffix.charAt(0);
+    return ret;
+  };
+
+  for (let row = 0; row < ROWS; ++row) {
+    for (let col = 0; col < COLS; ++col) {
+      if (backtrack(row, col, word)) return true;
+    }
+  }
+  return false;
+};
+
+
+
+
+
+
 
