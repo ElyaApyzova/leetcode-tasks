6acaf76b4ffcfefdc2d066c8928e1980e41a187a
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-07-29 23:07:03 +0300

    completed

--- app.js
+++ app.js
@@ -825,7 +825,24 @@ const exist = function (board, word) {
 
 
 
+//80 https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 
+
+//Input: nums = [1,1,1,2,2,3]
+//Output: 5, nums = [1,1,2,2,3,_]
+//Explanation: Your function should return k = 5, with the first five //elements of nums being 1, 1, 2, 2 and 3 respectively.
+//It does not matter what you leave beyond the returned k (hence they are //underscores).
+
+
+const removeDuplicates = function (nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (j < 2 || nums[i] > nums[j - 2]) {
+      nums[j++] = nums[i];
+    }
+  }
+  return j;
+};
 
 
 
