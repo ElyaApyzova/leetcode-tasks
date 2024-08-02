8f52732725ba4a01354a61188427a95c1a1efaff
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-08-02 23:04:13 +0300

    new task

--- app.js
+++ app.js
@@ -1018,3 +1018,19 @@ const partition = function (head, x) {
   before.next = after_head.next;
   return before_head.next;
 };
+
+
+// 87   https://leetcode.com/problems/scramble-string/description/
+
+
+//Input: s1 = "great", s2 = "rgeat"
+//Output: true
+//Explanation: One possible scenario applied on s1 is:
+//"great" --> "gr/eat" // divide at random index.
+//"gr/eat" --> "gr/eat" // random decision is not to swap the two //substrings and keep them in order.
+//"gr/eat" --> "g/r / e/at" // apply the same algorithm recursively on both //substrings. divide at random index each of them.
+//"g/r / e/at" --> "r/g / e/at" // random decision was to swap the first //substring and to keep the second substring in the same order.
+//"r/g / e/at" --> "r/g / e/ a/t" // again apply the algorithm recursively, //divide "at" to "a/t".
+//"r/g / e/ a/t" --> "r/g / e/ a/t" // random decision is to keep both //substrings in the same order.
+//The algorithm stops now, and the result string is "rgeat" which is s2.
+//As one possible scenario led s1 to be scrambled to s2, we return true.
