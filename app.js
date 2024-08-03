296fec77f2e7e078cfd2cb1f603083ae0927a5a4
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-08-03 23:14:06 +0300

    completed

--- app.js
+++ app.js
@@ -1089,5 +1089,46 @@ const mergeSorted = function (nums1, m, nums2, n) {
 };
 
 
+//89  https://leetcode.com/problems/gray-code/description/
 
+
+//Input: n = 2
+//Output: [0,1,3,2]
+//Explanation:
+//The binary representation of [0,1,3,2] is [00,01,11,10].
+//- 00 and 01 differ by one bit
+//- 01 and 11 differ by one bit
+//- 11 and 10 differ by one bit
+//- 10 and 00 differ by one bit
+//[0,2,3,1] is also a valid gray code sequence, whose binary representation //is [00,10,11,01].
+//- 00 and 10 differ by one bit
+//- 10 and 11 differ by one bit
+//- 11 and 01 differ by one bit
+//- 01 and 00 differ by one bit
+
+
+
+const grayCode = function (n) {
+  const res = [0]; 
+  const seen = new Set(res);
+  const helper = (n, res, seen) => {
+    if (res.length === Math.pow(2, n)) {
+      return true;
+    }
+    const curr = res[res.length - 1];
+    for (let i = 0; i < n; i++) {
+      const next = curr ^ (1 << i);
+      if (!seen.has(next)) {
+        seen.add(next);
+        res.push(next);
+        if (helper(n, res, seen)) return true;
+        seen.delete(next);
+        res.pop();
+      }
+    }
+    return false;
+  };
+  helper(n, res, seen);
+  return res;
+};
 
