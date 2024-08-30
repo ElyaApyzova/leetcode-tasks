a3ee81e69663ad2afefe8338420e1396dfbe7330
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-08-30 17:20:52 +0300

    completed

--- app.js
+++ app.js
@@ -1569,3 +1569,21 @@ const zigzagLevelOrder = function (root) {
   }
   return results;
 };
+
+
+//104 https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+
+const maxDepth = function (root) {
+  if (root === null) {
+    return 0;
+  }
+
+  const left_height = maxDepth(root.left);
+  const right_height = maxDepth(root.right);
+  return 1 + Math.max(left_height, right_height);
+};
+
