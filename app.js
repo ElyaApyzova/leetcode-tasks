4a5ba8ab093f4cc6b60db7b5d345064cd8fbe2e9
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-08-30 17:13:14 +0300

    completed

--- app.js
+++ app.js
@@ -1533,3 +1533,39 @@ const  levelOrder = function (root) {
   return levels;
 };
 
+
+//103  https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+
+
+
+//Input: root = [3,9,20,null,null,15,7]
+//Output: [[3],[20,9],[15,7]]
+
+
+const zigzagLevelOrder = function (root) {
+  if (root === null) return [];
+  const results = [];
+  const node_queue = [root, null];
+  const level_list = [];
+  let is_order_left = true;
+  while (node_queue.length > 0) {
+    const curr_node = node_queue.shift();
+    if (curr_node !== null) {
+      if (is_order_left) level_list.push(curr_node.val);
+
+      else level_list.unshift(curr_node.val);
+      if (curr_node.left !== null) node_queue.push(curr_node.left);
+
+      if (curr_node.right !== null) node_queue.push(curr_node.right);
+    
+    
+    } else {
+      results.push([...level_list]);
+      level_list.length = 0;
+      if (node_queue.length > 0) node_queue.push(null);
+
+      is_order_left = !is_order_left;
+    }
+  }
+  return results;
+};
