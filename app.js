9fbfbfc70c7d3591262bd7241134d91ce72f009c
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-03 23:06:07 +0300

    completed

--- app.js
+++ app.js
@@ -1966,6 +1966,34 @@ const connect = function (root) {
 };
 
 
+// 117 https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/editorial/
+
+
+// Input: root = [1,2,3,4,5,null,7]
+//Output: [1,#,2,3,#,4,5,7,#]
+//Explanation: Given the above binary tree (Figure A), your function //should populate each next pointer to point to its next right node, just //like in Figure B. The serialized output is in level order as connected //by the next pointers, with '#' signifying the end of each level.
+
+
+const Connect = function (root) {
+  if (!root) return root;
+  const Q = [];
+  Q.push(root);
+  while (Q.length > 0) {
+    const size = Q.length;
+    for (let i = 0; i < size; i++) {
+      const node = Q.shift();
+      if (i < size - 1) {
+        node.next = Q[0];
+      }
+      if (node.left) Q.push(node.left);
+      if (node.right) Q.push(node.right);
+    }
+  }
+  return root;
+};
+
+
+
 
 
 
