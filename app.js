48f153c58fcf2db022ba92c84ac7132983593810
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-03 23:18:26 +0300

    completed

--- app.js
+++ app.js
@@ -1993,6 +1993,31 @@ const Connect = function (root) {
 };
 
 
+// 118 https://leetcode.com/problems/pascals-triangle/editorial/
+
+
+//Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+
+const generate = function (numRows) {
+  const triangle = [];
+  triangle.push([]);
+  triangle[0].push(1);
+  for (let rowNum = 1; rowNum < numRows; rowNum++) {
+    const row = [];
+    const prevRow = triangle[rowNum - 1];
+    row.push(1);
+    for (let j = 1; j < rowNum; j++) {
+      row.push(prevRow[j - 1] + prevRow[j]);
+    }
+    row.push(1);
+    triangle.push(row);
+  }
+  return triangle;
+}
+
+
 
 
 
