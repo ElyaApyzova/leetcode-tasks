c648172d71287cc901698b210606f7ec9fc2f0cf
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-06 20:49:45 +0300

    completed

--- app.js
+++ app.js
@@ -2071,6 +2071,26 @@ const minimumTotal = function (triangle) {
 };
 
 
+//121  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), //profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because //you must buy before you sell.
+
+
+const maxProfit = function (prices) {
+  let minprice = Number.MAX_VALUE;
+  let maxprofit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minprice) minprice = prices[i];
+    else if (prices[i] - minprice > maxprofit)
+      maxprofit = prices[i] - minprice;
+  }
+  return maxprofit;
+};
+
 
 
 
