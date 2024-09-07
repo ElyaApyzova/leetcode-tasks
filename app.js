28115afdfcf3295d6e304c12a22a05c5f16ac307
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-07 23:11:28 +0300

    completed

--- app.js
+++ app.js
@@ -2151,6 +2151,39 @@ const maxPathProfit = function (prices) {
 };
 
 
+// 125 https://leetcode.com/problems/valid-palindrome/
+
+//Input: s = "A man, a plan, a canal: Panama"
+//Output: true
+//Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+const isPalindrome = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    while (i < j && !isLetterOrDigit(s[i])) {
+      i++;
+    }
+    while (i < j && !isLetterOrDigit(s[j])) {
+      j--;
+    }
+    if ((s[i] + "").toLowerCase() !== (s[j] + "").toLowerCase())
+      return false;
+    i++;
+    j--;
+  }
+  return true;
+};
+
+function isLetterOrDigit(character) {
+  const charCode = character.charCodeAt();
+  return (
+    (charCode >= "a".charCodeAt() && charCode <= "z".charCodeAt()) ||
+    (charCode >= "A".charCodeAt() && charCode <= "Z".charCodeAt()) ||
+    (charCode >= "0".charCodeAt() && charCode <= "9".charCodeAt())
+  );
+}
 
 
 
