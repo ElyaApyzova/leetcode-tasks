4553198cfb13670b9e5a6626cc12ffa5521fdbc7
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-09 22:41:10 +0300

    completed

--- app.js
+++ app.js
@@ -2385,6 +2385,40 @@ const solve = function (board) {
 };
 
 
+//131  https://leetcode.com/problems/palindrome-partitioning/description/
+
+//Input: s = "aab"
+//Output: [["a","a","b"],["aa","b"]]
+
+
+const Partition = function (s) {
+  const res = [];
+  dfs(s, [], res);
+  return res;
+  function dfs(s, path, res) {
+    if (!s.length) {
+      res.push(path);
+      return;
+    }
+    for (let i = 0; i < s.length; i++) {
+      const cur = s. substr(0, i + 1);
+      if (isPalindrome(cur)) {
+        dfs(s.substr(i + 1), path.concat(cur), res);
+      }
+    }
+  }
+  function isPalindrome(s) {
+    let lo = 0,
+    hi = s.length - 1;
+    while (lo < hi) {
+      if (s[lo++] != s[hi--]) return false;
+    }
+    return true;
+  }
+};
+
+
+
 
 
 
