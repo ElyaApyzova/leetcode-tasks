241212c26d0b1cc6be2e3deae5d8b0fce0f795a5
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-11 21:40:47 +0300

    completed

--- app.js
+++ app.js
@@ -2593,6 +2593,31 @@ const copyRandomList = function (head) {
 };
 
 
+//139 https://leetcode.com/problems/word-break/description/
+
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+//Output: true
+//Explanation: Return true because "leetcode" can be segmented as "leet //code".
+
+
+const wordBreak = function (s, wordDict) {
+  let words = new Set(wordDict);
+  let queue = [0];
+  let seen = new Set();
+  while (queue.length != 0) {
+    let start = queue.shift();
+    if (start == s.length) return true;
+    for (let end = start + 1; end <= s.length; end++) {
+      if (seen.has(end)) continue;
+      if (words.has(s.substring(start, end))) {
+        queue.push(end);
+        seen.add(end);
+      }
+    }
+  }
+  return false;
+};
 
 
 
