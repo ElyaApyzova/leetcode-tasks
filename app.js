3a5359e6434ae789e308056e14de241faa559f28
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-15 23:50:45 +0300

    completed

--- app.js
+++ app.js
@@ -2796,8 +2796,72 @@ const postorderTraversal = function (root) {
 
 
 
+//146 https://leetcode.com/problems/lru-cache/
+
+// Input
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//Output
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
 
 
+class Node {
+  constructer(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LRUCache {
+  constructer(capacity) {
+    this.capacity = capacity;
+    this.dic = new Map();
+    this.head = new Node(-1, -1);
+    this.tail = new Node(-1, -1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  get(key) {
+    if (!this.dic.has(key)) {
+      return -1;
+    }
+
+    let node = this.dic.get(key);
+    this.remove(node);
+    this.add(node);
+    return node.value;
+  }
+
+  put(key, value) {
+    if (this.dic.has(key)) {
+      this.remove(this.dic.get(key));
+    }
+    let node = new Node(key, value);
+    this.add(node);
+    this.dic.set(key, node);
+    if (this.dic.size > this.capacity) {
+      let nodeToDelete = this.head.next;
+      this.remove(nodeToDelete);
+      this.dic.delete(nodeToDelete.key);
+    }
+  }
+  add(node) {
+    let pre = this.tail.prev;
+    pre.next = node;
+    node.prev = pre;
+    node.next = this.tail;
+    this.tail.prev = node;
+  }
+  remove(node) {
+    let pre = node.prev;
+    let nxt = node.next;
+    pre.next = nxt;
+    nxt.prev = pre;
+  }
+}
 
 
 
