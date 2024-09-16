d96a58fd8d8ba9bbc2d7c44a28b867ce91d07cda
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-16 22:05:14 +0300

    completed

--- app.js
+++ app.js
@@ -2966,3 +2966,42 @@ const maxPoints = function (points) {
   }
   return result;
 };
+
+
+
+//150 https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+
+//Input: tokens = ["2","1","+","3","*"]
+//Output: 9
+//Explanation: ((2 + 1) * 3) = 9
+
+
+const OPERATORS = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "/": (a, b) => Math.trunc(a / b),
+  "*": (a, b) => a * b,
+};
+
+const evalRPN = function (tokens) {
+  let currentPosition = 0;
+
+  while (tokens.length > 1) {
+     
+    while (!(tokens[currentPosition] in OPERATORS)) {
+      currentPosition += 1;
+    }
+
+    const operator = tokens[currentPosition];
+    const operation = OPERATORS[operator];
+    const number1 = Number(tokens[currentPosition - 2]);
+    const number2 = Number(tokens[currentPosition - 1]);
+
+    tokens[currentPosition] = operation(number1, number2);
+
+    tokens.splice(currentPosition - 2, 2);
+    currentPosition -= 1;
+  }
+  return tokens[0];
+};
