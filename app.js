f9149ffc68a1fcaa5ecdd4ff4dd5bd8bd264679c
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-20 22:14:38 +0300

    completed

--- app.js
+++ app.js
@@ -3259,3 +3259,39 @@ const searches = function (nums, l, r) {
 
   return searches(nums, mid + 1, r);
 };
+
+
+//163 https://leetcode.com/problems/missing-ranges/
+
+//Input: nums = [-1], lower = -1, upper = -1
+//Output: []
+//Explanation: There are no missing ranges since there are no missing numbers.
+
+
+
+const findMissingRanges = function (nums, lower, upper) {
+  let n = nums.length;
+  let missingRanges = [];
+  if (n === 0) {
+    missingRanges.push([lower, upper]);
+    return missingRanges;
+  }
+
+  if (lower < nums[0]) {
+    missingRanges.push([lower, nums[0] - 1]);
+  }
+
+  for (let i = 0; i < n - 1; i++) {
+    if (nums[i + 1] - nums[i] <= 1) {
+      continue;
+    }
+    missingRanges.push([nums[i] + 1, nums[i + 1] - 1]);
+  }
+
+  if (upper > nums[n - 1]) {
+    missingRanges.push([nums[n - 1] + 1, upper]);
+  }
+
+  return missingRanges;
+};
+
