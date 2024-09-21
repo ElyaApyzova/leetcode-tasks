ce55cf1f627364fc88734fb97da7e7f5b178d0ce
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-21 22:33:16 +0300

    completed

--- app.js
+++ app.js
@@ -3388,3 +3388,29 @@ const fractionToDecimal = function (numerator, denominator) {
   }
   return fraction.join("");
 };
+
+
+
+//167 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+// Input: numbers = [2,7,11,15], target = 9
+//Output: [1,2]
+//Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+
+const twoSum = function (numbers, target) {
+  let low = 0;
+  let high = numbers.length - 1;
+  while (low < high) {
+    let sum = numbers[low] + numbers[high];
+
+    if (sum == target) {
+      return [low + 1, high + 1];
+    } else if (sum < target) {
+      low++;
+    } else {
+      high--;
+    }
+  }
+  return [-1, -1];
+};
