37a3820ba29f8d894a2aa2899d89f2762aeb1aed
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-22 21:31:05 +0300

    completed

--- app.js
+++ app.js
@@ -3457,3 +3457,43 @@ const majorityElement = function (nums) {
   }
   return 0;
 };
+
+
+//170 https://leetcode.com/problems/two-sum-iii-data-structure-design/
+
+//Input
+//["TwoSum", "add", "add", "add", "find", "find"]
+//[[], [1], [3], [5], [4], [7]]
+//Output
+//[null, null, null, null, true, false]
+
+
+class TwoSum {
+  constructor() {
+    this.nums = [];
+    this.is_sorted = false;
+  }
+
+  add(number) {
+    this.nums.push(number);
+    this.is_sorted = false;
+  }
+
+  find(value) {
+    if (!this.is_sorted) {
+      this.nums.sort((a, b) => a - b);
+      this.is_sorted = true;
+    }
+
+    let low = 0,
+    high = this.nums.length - 1;
+    while (low < high) {
+      let twosum = this.nums[low] + this.nums[high];
+      if (twosum < value) low += 1;
+      else if (twosum > value) high -= 1;
+      else return true;
+    }
+    return false;
+  }
+}
+
