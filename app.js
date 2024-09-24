f974c13e919e832424cb641b860722d71c731a45
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-09-24 21:00:34 +0300

    completed

--- app.js
+++ app.js
@@ -3701,3 +3701,20 @@ function calculateMinimumHP(dungeon) {
 
 
 
+//179 https://leetcode.com/problems/largest-number/
+
+//Input: nums = [10,2]
+//Output: "210"
+
+
+class Solution {
+  largestNumber(nums) {
+    const strNums = nums.map(String);
+    strNums.sort((a, b) => (b + a).localeCompare(a + b));
+
+    if (strNums[0] === "0") {
+      return "0";
+    }
+    return strNums.join('');
+  }
+}
