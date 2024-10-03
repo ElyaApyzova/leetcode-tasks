eb7cd303ebc0c230d69ccb8a3f035db968f90e9d
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-03 22:07:22 +0300

    completed

--- app.js
+++ app.js
@@ -4680,6 +4680,31 @@ const words = ["oath", "pea", "eat", "rain"];
 console.log(solution.findWords(board, words));  // Output: ["oath", "eat"]
 
 
+//256 https://leetcode.com/problems/paint-house/description/
+
+// Input: costs = [[17,2,17],[16,16,5],[14,3,19]]
+//Output: 10
+//Explanation: Paint house 0 into blue, paint house 1 into green, paint house 2 into blue.
+//Minimum cost: 2 + 5 + 3 = 10.
+
+
+class Solution {
+  minCost(costs) {
+    const n = costs.length;
+    const dp = Array.from({ length: n }, () => Array(3).fill(0));
+    dp[0] = costs[0].slice();
+
+    for (let i = 1; i < n; i++) {
+      dp[i][0] = costs[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+      dp[i][1] = costs[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+      dp[i][2] = costs[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+    }
+    return Math.min(dp[n-1][0], dp[n-1][1], dp[n-1][2]);
+  }
+}
+
+
+
 
 
 
