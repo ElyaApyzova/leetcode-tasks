6aa19a63a456b3a8b50079012ca67fb667db1408
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-10-05 22:14:52 +0300

    completed

--- app.js
+++ app.js
@@ -4801,6 +4801,35 @@ const target = 2;
 console.log(solution.threeSumSmaller(nums, target)); // Вывод: 2
 
 
+// 260 https://leetcode.com/problems/single-number-iii/description/
+
+//Input: nums = [1,2,1,3,2,5]
+//Output: [3,5]
+//Explanation:  [5, 3] is also a valid answer.
+
+
+class Solution {
+  singleNumber(nums) {
+let xor = 0
+for (const num of nums) {
+  xor ^= num
+}
+
+const diff = xor & -xor
+const res = [0, 0]
+for (const num of nums) {
+  if ((num & diff) === 0) {
+    res[0] ^= num
+  } else {
+    res[1] ^= num
+  }
+}
+return res
+  }
+}
+
+
+
 
 
 
